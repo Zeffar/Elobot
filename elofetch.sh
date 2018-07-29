5035526ee095a767c690beb70a01f8763d531797
Wesalius <Wesalius@users.noreply.github.com>
2018-07-29 08:14:06 +0200

    Update elofetch.sh

--- elofetch.sh
+++ elofetch.sh
@@ -13,17 +13,17 @@ printf "Did you update wd-triplets-quiet.py (y/n)? "
 read answer
 if [ "$answer" != "${answer#[Yy]}" ]; then
   printf "Fetching file from ratings.fide.com.\n"
-  wget -q http://ratings.fide.com/download/standard_$1frl_xml.zip
-  unzip -q standard_$1frl_xml.zip
+  wget -q http://ratings.fide.com/download/standard_"$1"frl_xml.zip
+  unzip -q standard_"$1"frl_xml.zip
 
   printf "Extracting data.\n"
-  xmlstarlet sel -t -m "//fideid" -v . -n standard_$1frl_xml.xml > ids.txt
-  xmlstarlet sel -t -m "//rating" -v . -n standard_$1frl_xml.xml > ratings.txt
+  xmlstarlet sel -t -m "//fideid" -v . -n standard_"$1"frl_xml.xml > ids.txt
+  xmlstarlet sel -t -m "//rating" -v . -n standard_"$1"frl_xml.xml > ratings.txt
   paste -d , ids.txt ratings.txt > pairs.csv
   python3 wd-triplets-quiet.py
 
   printf "Cleaning up.\n"
-  rm pairs.csv ids.txt ratings.txt standard_$1frl_xml.zip standard_$1frl_xml.xml
+  rm pairs.csv ids.txt ratings.txt standard_"$1"frl_xml.zip standard_"$1"frl_xml.xml
 
   printf "Output can be found at elo-item-rating-triplet.csv and item-fide-id-match-not-found.csv\n"
 else
